982323b0ac67840955991f469d916876172a2211
Abdul Fattah Ismail <72433456+Abdulfi2@users.noreply.github.com>
2021-11-20 13:04:07 +0700

    Update 2021_11_17_045311_create_pasien_table.php

--- projek/REST-API-Covid/database/migrations/2021_11_17_045311_create_pasien_table.php
+++ projek/REST-API-Covid/database/migrations/2021_11_17_045311_create_pasien_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePatientsTable extends Migration
+class CreatePasienTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePatientsTable extends Migration
      */
     public function up()
     {
-        Schema::create('patients', function (Blueprint $table) {
+        Schema::create('pasien', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->string('phone');
