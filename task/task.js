4c3154983ad112ae44b7c5ee6b73d5e355c72e69
Abdul Fattah Ismail <72433456+Abdulfi2@users.noreply.github.com>
2021-12-14 12:57:30 +0700

    Add files via upload

---
+++ task/task.js
@@ -0,0 +1,35 @@
+/**
+ * Fungsi untuk menampilkan hasil download
+ * @param {string} result - Nama file yang didownload
+ */
+  const showDownload = (result) => {
+    let ket1 = 'Download selesai';
+    let ket2 = 'Hasil Download : ' + result;
+  };
+  /**
+   * Fungsi untuk download file
+   * @param {function} callback - Function callback show
+   */
+  const download = () => {
+    return new Promise(function(resolve) {
+      setTimeout(function ()  {
+        const result = `windows-10.exe1`;
+        resolve(result);
+      }, 3000);
+    });
+  };
+  
+  async function main () {
+    const $result = await download();
+    showDownload ($result);
+      console.log("Download selesai");
+      console.log("Hasil download" + $result)
+  };
+  main();
+  
+  /**
+   * TODO:
+   * - Refactor callback ke Promise atau Async Await
+   * - Refactor function ke ES6 Arrow Function
+   * - Refactor string ke ES6 Template Literals
+   */
