ad5a04f62aad316d5020babdc68aeda3b85a03ed
Abdul Fattah Ismail <72433456+Abdulfi2@users.noreply.github.com>
2021-12-22 21:26:56 +0700

    Delete pertemuan10 directory

--- pertemuan10/synchronous.js
+++
@@ -1,9 +0,0 @@
-console.log("Abdul Membuka Bowser Google Chrome.");
-
-console.log(`
-Abdul download file selama 1 jam.
-  Downloading 1 Hour ...
-  Download Complete.
-`);
-
-console.log("Abdul membuka Youtube.");
