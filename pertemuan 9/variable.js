1c682d48ec27236c0a41429d12e2348bd9dd9ee5
Abdul Fattah Ismail <72433456+Abdulfi2@users.noreply.github.com>
2021-12-22 21:48:57 +0700

    Delete pertemuan 9 directory

--- pertemuan 9/variable.js
+++
@@ -1,9 +0,0 @@
-/**
- * Membuat variable dengan keyword const.
- * Variable (const) tidak dapat diubah nilainya.
- */
- const name = "Abdul Fattah Ismail";
- const age = 20;
- const major = "Teknik Informatika";
- 
- console.log(name, age, major);
